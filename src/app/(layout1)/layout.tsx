07fe4264f9d2253c5b9ce91043b86d1b139a691b
Ritesh Bucha <bucharitesh@gmail.com>
2022-12-09 03:54:54 +0530

    chore: add smooth scroll

--- src/app/(layout1)/layout.tsx
+++ src/app/(layout1)/layout.tsx
@@ -1,14 +1,38 @@
+"use client";
+
 import { Main } from '@/templates/Main';
 import Navbar from '@/shared/Navbar';
 import Footer from '@/shared/Footer';
+import { gsap } from 'gsap-trial';
+import ScrollTrigger from 'gsap-trial/ScrollTrigger';
+import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+import { useLayoutEffect, useRef } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const el = useRef(null);
+  // const q = gsap.utils.selector(el);
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  useLayoutEffect(() => {
+    const smoother = ScrollSmoother.create({
+      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+      effects: true, // looks for data-speed and data-lag attributes on elements
+      smoothTouch: 0.1,
+    });
+    return () => {
+      smoother.kill();
+    };
+  }, []);
+
+  
   return (
     <Main title="" description="">
-      <div>
-        <Navbar />
-        {children}
-        <Footer />
+      <div ref={el} id="smooth-wrapper">
+        <div id="smooth-content">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </div>
     </Main>
   );
