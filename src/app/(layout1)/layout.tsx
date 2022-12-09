7d080bdc551a64edcf2db13f57f78f5f3360129a
Yuvraj Singh Chouhan <yuvrajisbest13@gmail.com>
2022-12-09 12:03:36 +0530

    style(all): fix scroll

--- src/app/(layout1)/layout.tsx
+++ src/app/(layout1)/layout.tsx
@@ -3,37 +3,37 @@
 import { Main } from '@/templates/Main';
 import Navbar from '@/shared/Navbar';
 import Footer from '@/shared/Footer';
-import { gsap } from 'gsap-trial';
-import ScrollTrigger from 'gsap-trial/ScrollTrigger';
-import ScrollSmoother from 'gsap-trial/ScrollSmoother';
-import { useLayoutEffect, useRef } from 'react';
+// import { gsap } from 'gsap-trial';
+// import ScrollTrigger from 'gsap-trial/ScrollTrigger';
+// import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+// import { useLayoutEffect, useRef } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const el = useRef(null);
+  // const el = useRef(null);
   // const q = gsap.utils.selector(el);
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-  useLayoutEffect(() => {
-    const smoother = ScrollSmoother.create({
-      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-      effects: true, // looks for data-speed and data-lag attributes on elements
-      smoothTouch: 0.1,
-    });
-    return () => {
-      smoother.kill();
-    };
-  }, []);
+  // useLayoutEffect(() => {
+  //   const smoother = ScrollSmoother.create({
+  //     smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  //     effects: true, // looks for data-speed and data-lag attributes on elements
+  //     smoothTouch: 0.1,
+  //   });
+  //   return () => {
+  //     smoother.kill();
+  //   };
+  // }, []);
 
   
   return (
     <Main title="" description="">
-      <div ref={el} id="smooth-wrapper">
-        <div id="smooth-content">
+      {/* <div ref={el} id="smooth-wrapper"> */}
+        {/* <div id="smooth-content"> */}
           <Navbar />
           {children}
           <Footer />
-        </div>
-      </div>
+        {/* </div> */}
+      {/* </div> */}
     </Main>
   );
 }
